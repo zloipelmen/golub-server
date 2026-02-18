85b68119c120b71417d0fd6496f550dd63f64671
zloipelmen <zloipelmen@yandex.ru>
2026-02-18 15:34:35 +0300

    Rename WS event types to canonical names

--- internal/ws/types.go
+++ internal/ws/types.go
@@ -2,12 +2,12 @@ package ws
 
 const (
 	TypeAuth            = "auth"
-	TypeAuthOK          = "auth_ok"
-	TypeSendMessage     = "send_message"
-	TypeSendMessageOK   = "send_message_ok"
-	TypeMessage         = "message"
+	TypeAuthOK          = "auth.ok"
+	TypeSendMessage     = "message.send"
+	TypeSendMessageOK   = "message.sent"
+	TypeMessage         = "message.new"
 	TypeSync            = "sync"
-	TypeSyncOK          = "sync_ok"
+	TypeSyncOK          = "sync.result"
 	TypeError           = "error"
 )
 
